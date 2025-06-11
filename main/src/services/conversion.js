07e3a2f966c0f394a5e90c134f0cd9b7dad26fc9
Xqni <jxesuki@gmail.com>
2025-06-11 13:12:26 -0500

    add proxy

--- main/src/services/conversion.js
+++ main/src/services/conversion.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/'
+const baseUrl = '/'
 
 const sendreq = async (unit, object) => {
     const request = axios.post(`${baseUrl}${unit}`, object)
