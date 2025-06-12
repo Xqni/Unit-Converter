bd4d30ae28c544bef9edf5473f5455f49a766f91
Xqni <jxesuki@gmail.com>
2025-06-11 20:09:57 -0500

    fix typo in vite.config.js

--- main/vite.config.js
+++ main/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // if backend deployed on internet, forwards to requests to local port for dev purposes
+  // if backend deployed on internet, forwards requests to local port for dev purposes
   // needs to have dev env run, npm run dev
   server: {
     proxy: {
